a663d4e05afc1c99d113025fd5b72c470d973551
JUN JIAN SHEN <jshen9085@gmail.com>
2018-07-25 01:40:14 +0000

    automatically backed up by learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,26 @@
+def using_push(array, element)
+  array.push(element)
+  return array
+end 
+
+def using_unshift(array, string)
+  array.unshift(string)
+  return array
+end 
+
+def using_pop(array)
+  last = array.last 
+  array.pop 
+  return last
+end 
+
+def pop_with_args(array)
+  array = array.pop 
+  array = array.pop
+  return array
+end 
+
+def using_shift(array)
+  array = array.shift 
+  return array
+end 
